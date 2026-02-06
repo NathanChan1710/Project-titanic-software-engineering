d464a41987aa63ee09c8f76334f5d6982a1b253b
Camille <camillefr2@gmail.com>
2026-02-06 18:04:54 +0100

    Supp + déplacement

--- titanic/titanic_project/__init__.py
+++
@@ -1,4 +0,0 @@
-# laisser en vide, pas utile
-# ca sert à organiser comment le code s'éxécute
-
-#normalement un fichier main devrait exister, qui s'execute tout a la fois, mais dans notre cas on va pas le faire
